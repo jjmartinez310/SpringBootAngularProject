8cdb778523c6c3012533945d1003f642e9163adb
jjmartinez310 <108151748+jjmartinez310@users.noreply.github.com>
2022-07-18 17:41:14 -0500

    First Commit

---
+++ telecomproject/src/main/java/com/groupproject/telecomproject/TelecomprojectApplication.java
@@ -0,0 +1,15 @@
+package com.groupproject.telecomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TelecomprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomprojectApplication.class, args);
+	}
+
+}
